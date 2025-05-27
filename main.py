bf5dd482810f07320341b47ee195db123a0b3d94
Anton-Shcherba <tony.shcherba@gmail.com>
2025-05-27 14:23:56 +0300

    fix: adjust incorrect duration value

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import asyncio
-import logging
-
 from aiogram import F
 from aiogram.types import Message
 from aiogram import Bot, Dispatcher, html
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
-
 from aiogram.types import InputMediaPhoto, InputMediaVideo
 from utils import (
     fetch_cobalt_links,
@@ -41,52 +38,43 @@ async def echo_handler(message: Message) -> None:
     res = [item for url in urls if (el := await fetch_cobalt_links(url)) for item in el]
 
     if res:
-        answer = await message.reply("есть результаты, пробую скачать")
+        status_msg = await message.reply("⏳ Обработка медиафайлов...")
         try:
-            if len(res) == 1:
-                url = res[0]
-                async with fetch_to_buffer_async(url[1]) as buffer:
-                    if url[0] == "video":
-                        async with get_video_info_from_buffer_async(buffer) as params:
-                            await message.reply_video(**params)
-                    elif url[0] == "photo":
-                        await message.reply_photo(
-                            BufferedInputFile(buffer.read(), filename="photo.jpg")
-                        )
-
-            elif len(res) > 1:
-                for i in range(0, len(res), 10):
-                    batch = res[i : i + 10]
-                    media_group = []
-                    for item in batch:
-                        async with fetch_to_buffer_async(item[1]) as buffer:
-                            if item[0] == "video":
-                                async with get_video_info_from_buffer_async(
-                                    buffer
-                                ) as params:
-                                    media_group.append(
-                                        InputMediaVideo(
-                                            media=params["video"],
-                                            duration=params["duration"],
-                                            width=params["width"],
-                                            height=params["height"],
-                                            thumbnail=params["thumbnail"],
-                                        )
-                                    )
-                            elif item[0] == "photo":
+            size = len(res)
+            for i in range(0, len(res), 10):
+                batch = res[i : i + 10]
+                media_group = []
+                for item in batch:
+                    async with fetch_to_buffer_async(item[1]) as buffer:
+                        if item[0] == "video":
+                            async with get_video_info_from_buffer_async(
+                                buffer
+                            ) as params:
+                                if size == 1:
+                                    await message.reply_video(**params)
+                                    return
                                 media_group.append(
-                                    InputMediaPhoto(
-                                        media=BufferedInputFile(
-                                            buffer.read(), filename="photo.jpg"
-                                        )
+                                    InputMediaVideo(
+                                        media=params["video"],
+                                        duration=params["duration"],
+                                        width=params["width"],
+                                        height=params["height"],
+                                        thumbnail=params["thumbnail"],
                                     )
                                 )
-                    if media_group:
-                        await message.reply_media_group(media_group)
+                        elif item[0] == "photo":
+                            file = BufferedInputFile(
+                                buffer.read(), filename="photo.jpg"
+                            )
+                            if size == 1:
+                                await message.reply_photo(file)
+                                return
+                            media_group.append(InputMediaPhoto(media=file))
+                await message.reply_media_group(media_group)
         except Exception:
-            await message.reply("что-то пошло не так")
+            await message.reply("❌ Ошибка")
         finally:
-            await answer.delete()
+            await status_msg.delete()
 
 
 async def main() -> None:
@@ -95,5 +83,4 @@ async def main() -> None:
 
 
 if __name__ == "__main__":
-    # logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
--- utils.py
+++ utils.py
@@ -1,13 +1,10 @@
-import httpx
-import aiofiles
-
+from httpx import AsyncClient
+from aiofiles import tempfile
 import asyncio
-import aiohttp
+from aiohttp import ClientSession, ClientTimeout
 from typing import List, Tuple, Optional, Literal, Dict
-
-
 from contextlib import asynccontextmanager
-import io
+from io import BytesIO
 import cv2
 from aiogram.types import BufferedInputFile
 
@@ -20,9 +17,9 @@ async def fetch_to_buffer_async(url: str, max_size_mb: int = 50):
         "Accept-Encoding": "gzip, deflate, br",
     }
     MAX_SIZE_BYTES = max_size_mb * 1024 * 1024
-    buffer = io.BytesIO()
+    buffer = BytesIO()
     try:
-        async with httpx.AsyncClient(timeout=30.0) as client:
+        async with AsyncClient(timeout=30.0) as client:
             if head := await client.head(url, headers=HEADERS, follow_redirects=True):
                 if int(head.headers.get("Content-Length", 0)) > MAX_SIZE_BYTES:
                     raise ValueError("Size limit exceeded")
@@ -46,23 +43,21 @@ async def fetch_to_buffer_async(url: str, max_size_mb: int = 50):
 
 
 @asynccontextmanager
-async def get_video_info_from_buffer_async(buffer: io.BytesIO):
+async def get_video_info_from_buffer_async(buffer: BytesIO):
     def sync_video_analysis(video_path: str):
         cap = cv2.VideoCapture(video_path)
         try:
             width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
             height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
             fps = cap.get(cv2.CAP_PROP_FPS)
-            frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-            duration = int(frame_count / fps) if fps > 0 else 0
-            success, first_frame = cap.read()
-            if not success:
-                raise ValueError("Failed to read video frame")
+            _, first_frame = cap.read()
+            frame_count = sum(1 for _ in iter(cap.grab, False))
+            duration = round(frame_count / fps) if fps > 0 else 0
             return duration, width, height, first_frame
         finally:
             cap.release()
 
-    async with aiofiles.tempfile.NamedTemporaryFile("wb") as temp_file:
+    async with tempfile.NamedTemporaryFile("wb") as temp_file:
         await temp_file.write(buffer.read())
         buffer.seek(0)
         duration, width, height, frame = await asyncio.to_thread(
@@ -78,57 +73,6 @@ async def get_video_info_from_buffer_async(buffer: io.BytesIO):
                 buffer_img.tobytes(), filename="thumbnail.jpg"
             ),
         }
-
-
-# async def fetch_and_save(url: str, filename: str, max_size_mb: int = 50) -> None:
-#     HEADERS = {
-#         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36",
-#         "Accept": "*/*",
-#         "Accept-Encoding": "gzip, deflate, br",
-#     }
-
-#     MAX_SIZE_BYTES = max_size_mb * 1024 * 1024
-
-#     async with httpx.AsyncClient(timeout=30.0) as client:
-#         try:
-#             # Проверка размера через HEAD
-#             head_response = await client.head(
-#                 url, headers=HEADERS, follow_redirects=True
-#             )
-#             head_response.raise_for_status()
-
-#             content_length = head_response.headers.get("Content-Length")
-#             if content_length and int(content_length) > MAX_SIZE_BYTES:
-#                 raise ValueError(f"File size exceeds limit ({max_size_mb} MB)")
-
-#             # Потоковая загрузка с контролем размера
-#             downloaded = 0
-#             async with client.stream(
-#                 "GET", url, headers=HEADERS, follow_redirects=True
-#             ) as response:
-#                 response.raise_for_status()
-
-#                 # Создаем папку если не существует
-#                 Path(filename).parent.mkdir(parents=True, exist_ok=True)
-
-#                 async with aiofiles.open(filename, "wb") as file:
-#                     async for chunk in response.aiter_bytes():
-#                         await file.write(chunk)
-#                         downloaded += len(chunk)
-#                         if downloaded > MAX_SIZE_BYTES:
-#                             raise ValueError(
-#                                 f"File size exceeded limit ({max_size_mb} MB)"
-#                             )
-
-#             # Файл успешно сохранен
-
-#         except httpx.HTTPStatusError as e:
-#             raise ValueError(f"HTTP error: {e.response.status_code}") from e
-#         except Exception as e:
-#             # Удаляем частично загруженный файл при ошибке
-#             if Path(filename).exists():
-#                 Path(filename).unlink()
-#             raise ValueError(f"Download failed: {str(e)}") from e
 
 
 def determine_media_type(
@@ -174,9 +118,7 @@ async def fetch_cobalt_links(
     headers = {"Accept": "application/json", "Content-Type": "application/json"}
     payload = {"url": media_url, "videoQuality": "720", "downloadMode": "auto"}
 
-    async with aiohttp.ClientSession(
-        timeout=aiohttp.ClientTimeout(total=timeout)
-    ) as session:
+    async with ClientSession(timeout=ClientTimeout(total=timeout)) as session:
         async with session.post(api_url, headers=headers, json=payload) as response:
             # if response.status != 200:
             #     error_text = await response.text()
@@ -193,71 +135,3 @@ async def fetch_cobalt_links(
             #     raise ValueError(f"Cobalt API error: {data['error']['code']}")
             # raise ValueError(f"Unknown response status: {data['status']}")
             return None
-
-
-# def calculate_video_params(direct_link: str):
-#     with VideoFileClip(direct_link) as video:
-#         duration = int(video.duration)
-#         width, height = video.size
-
-#         # Создание миниатюры с использованием временного файла
-#         with tempfile.NamedTemporaryFile(suffix=".jpg", delete=False) as temp_thumb:
-#             frame = video.get_frame(int(duration / 2))  # Кадр из середины видео
-#             image = Image.fromarray(frame)
-#             image.thumbnail((320, 320), Image.Resampling.LANCZOS)
-
-#             # Сохранение миниатюры с контролем качества
-#             quality = 85
-#             while True:
-#                 # Уменьшаем качество, если файл слишком большой
-#                 image.save(temp_thumb.name, format="JPEG", quality=quality)
-#                 if os.path.getsize(temp_thumb.name) <= 200 * 1024:
-#                     break
-#                 quality = max(10, quality - 5)
-#             print(temp_thumb.name)
-#             return {
-#                 "video": FSInputFile(direct_link),
-#                 "duration": duration,
-#                 "width": width,
-#                 "height": height,
-#                 "thumbnail": FSInputFile(temp_thumb.name),
-#             }
-
-
-# def syncfunc():
-#     async def asyncfunc():
-#         links = await fetch_cobalt_links("https://www.youtube.com/watch?v=obc6n9U0s1E")
-#         for link in links:
-#             print(link)
-#             await fetch_and_save(
-#                 url=link[1],
-#                 filename=f"{datetime.now().strftime('%Y%m%d%H%M%S%f')}.{'jpg' if link[0] == 'photo' else 'mp4'}",
-#                 max_size_mb=50,
-#             )
-
-#     asyncio.run(asyncfunc())
-
-
-# syncfunc()
-
-
-# async def process_downloaded_file():
-#     url = "https://drive.usercontent.google.com/u/0/uc?id=1c3GLj3RbRm65A4R21NhBbOX2FjjcrYNf&export=download"  # Замените на реальный URL
-
-#     try:
-#         # Используем контекстный менеджер для загрузки
-#         async with fetch_and_save_async(url) as file:
-#             print(f"file: {file.name}")
-#             file_size = Path(str(file.name)).stat().st_size
-#             print(f"size: {file_size / (1024 * 1024):.2f} MB")
-#             async with get_video_info_async(str(file.name)) as param:
-#                 print(param)
-
-#     except ValueError as e:
-#         print(f"Ошибка загрузки: {e}")
-#     except Exception as e:
-#         print(f"Неожиданная ошибка: {e}")
-
-
-# # Запуск
-# asyncio.run(process_downloaded_file())
